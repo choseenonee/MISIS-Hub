a9ecb333e22978a331493a43a7a5727d4c50791a
choseenonee <okaymans44@gmail.com>
2023-09-08 23:39:01 +0300

    added GPT module

---
+++ backend/gpt_tag_validation.py
@@ -0,0 +1,47 @@
+import os
+from typing import List
+
+import openai
+from dotenv import load_dotenv
+
+
+dotenv_relative_path = '../.env'
+dotenv_path = os.path.abspath(dotenv_relative_path)
+load_dotenv(dotenv_path=dotenv_path)
+
+openai.api_key = os.getenv('GPT_API_KEY')
+
+
+def send_prompt(prompt):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        temperature=0.6,
+        max_tokens=100,
+        n=1,
+        stop=None
+    )
+
+    return response.choices[0].text.strip()
+
+
+def validate_tag(tag: str) -> bool:
+    prompt = f'''Ответь да, если {tag} может быть тегом в анкете для знакомств, или нет. Под тэгом я имею ввиду hard, soft skills, а так же чьи-то интересы, хобби, предпочтения. Отвечай только да или нет.'''
+
+    answer = send_prompt(prompt)
+
+    answer = answer.lower()
+    if 'yes' in answer or 'да' in answer.lower():
+        return True
+    else:
+        return False
+
+
+def convert_to_tags(text: str) -> List[str]:
+    prompt = f'''Проанализируй текст, разбей его на теги, которые могут означать hard, soft skills, интересы, хобби, предпочтения в анкете для знакомств: {text}. 
+    В своём ответе укажи только теги с большой буквы через пробел, не используй никаких других дополнительных слов.'''
+
+    answer = send_prompt(prompt)
+    maybe_tags = answer.split(' ')
+    validated_tags = [tag for tag in maybe_tags if validate_tag(tag)]
+    return validated_tags
